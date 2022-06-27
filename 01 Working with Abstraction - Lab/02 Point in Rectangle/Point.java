74835cefed6d33d0c8e0f5ddd5279396b6b24af8
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-06-27 12:26:07 +0300

    Create Point.java

---
+++ 01 Working with Abstraction - Lab/02 Point in Rectangle/Point.java
@@ -0,0 +1,19 @@
+package pointInRectangle;
+
+public class Point {
+    private int X;
+    private int Y;
+
+    public Point(int coordinateX, int coordinateY) {
+        this.X = coordinateX;
+        this.Y = coordinateY;
+    }
+
+    public int getX() {
+        return X;
+    }
+
+    public int getY() {
+        return Y;
+    }
+}
