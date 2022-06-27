eb6dc65ff51277dd308544cb87eba60a588cdfbd
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-06-27 15:27:11 +0300

    Create Season.java

---
+++ 01 Working with Abstraction - Lab/04 Hotel Reservation/Season.java
@@ -0,0 +1,18 @@
+package hotelReservation;
+
+public enum Season {
+    Autumn(1),
+    Spring(2),
+    Winter(3),
+    Summer(4);
+
+    private int multiplayer;
+
+    Season(int multiplayer) {
+        this.multiplayer = multiplayer;
+    }
+
+    public int getMultiplayer() {
+        return multiplayer;
+    }
+}
