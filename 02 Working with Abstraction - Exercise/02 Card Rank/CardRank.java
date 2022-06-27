48c0958f1092717bdcf0d45b93b8d592a4ee0d69
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-06-27 16:10:40 +0300

    Create CardRank.java

---
+++ 02 Working with Abstraction - Exercise/02 Card Rank/CardRank.java
@@ -0,0 +1,36 @@
+package cardRank;
+
+public enum CardRank {
+    ACE(0),
+    TWO(1),
+    THREE(2),
+    FOUR(3),
+    FIVE(4),
+    SIX(5),
+    SEVEN(6),
+    EIGHT(7),
+    NINE(8),
+    TEN(9),
+    JACK(10),
+    QUEEN(11),
+    KING(12);
+
+    private int value;
+
+    CardRank(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        //Ordinal value: 0; Name value: CLUBS
+        return "Ordinal value: " + value + "; Name value: ";
+    }
+
+    public static void printAll() {
+        System.out.println("Card Ranks:");
+        for (CardRank value : CardRank.values()) {
+            System.out.println(value + value.name());
+        }
+    }
+}
