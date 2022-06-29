2536900b1cf6b07d9ce499aa2331c4c8de081483
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-06-29 15:33:49 +0300

    Update Main.java

--- 03 Encapsulation - Lab/04 First And Reserve Team/Main.java
+++ 03 Encapsulation - Lab/04 First And Reserve Team/Main.java
@@ -8,8 +8,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         int numberOfPeople = Integer.parseInt(console.nextLine());
-        List<Person> players = new ArrayList<>();
-
+        Team team = new Team("Black eagles");
+        
         while (numberOfPeople-- > 0) {
             String[] input = console.nextLine().split("\\s+");
 
@@ -19,11 +19,8 @@ public class Main {
             double salary = Double.parseDouble(input[3]);
 
             Person playerToAdd = new Person(firstName, lastName, age, salary);
-            players.add(playerToAdd);
+            team.addPlayer(playerToAdd);
         }
-
-        Team team = new Team("Black eagles");
-        players.forEach(team::addPlayer);
 
         System.out.println("First team have " + team.getFirstTeam().size() + " players");
         System.out.println("Reserve team have " + team.getReserveTeam().size() + " players");
