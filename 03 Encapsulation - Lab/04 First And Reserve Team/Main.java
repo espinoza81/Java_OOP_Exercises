6dcb2dbb46f22eb65cebe8e9a9dea38f850ccc62
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-06-29 15:25:15 +0300

    Create Main.java

---
+++ 03 Encapsulation - Lab/04 First And Reserve Team/Main.java
@@ -0,0 +1,31 @@
+package firstAndReserveTeam;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int numberOfPeople = Integer.parseInt(console.nextLine());
+        List<Person> players = new ArrayList<>();
+
+        while (numberOfPeople-- > 0) {
+            String[] input = console.nextLine().split("\\s+");
+
+            String firstName = input[0];
+            String lastName = input[1];
+            int age = Integer.parseInt(input[2]);
+            double salary = Double.parseDouble(input[3]);
+
+            Person playerToAdd = new Person(firstName, lastName, age, salary);
+            players.add(playerToAdd);
+        }
+
+        Team team = new Team("Black eagles");
+        players.forEach(team::addPlayer);
+
+        System.out.println("First team have " + team.getFirstTeam().size() + " players");
+        System.out.println("Reserve team have " + team.getReserveTeam().size() + " players");
+    }
+}
