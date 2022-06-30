5d5353608dc41cb2b195f7cb43d96322f530f6f3
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-06-30 10:06:10 +0300

    Create Person.java

---
+++ 04 Encapsulation - Exercises/03 Shopping Spree/Person.java
@@ -0,0 +1,52 @@
+package shoppingSpree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Person {
+    private String name;
+    private double money;
+    private List<Product> products;
+
+    public Person(String name, double money) {
+        this.setName(name);
+        this.setMoney(money);
+        this.products = new ArrayList<>();
+    }
+
+    private void setName(String name) {
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+        this.name = name;
+    }
+
+    private void setMoney(double money) {
+        if (money < 0) {
+            throw new IllegalArgumentException("Money cannot be negative");
+        }
+        this.money = money;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void buyProduct(Product product) {
+        if (product.getCost() < money) {
+            money -= product.getCost();
+            products.add(product);
+        } else {
+            throw new IllegalArgumentException(name + " can't afford " + product.getName());
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (products.isEmpty()) {
+            return name + " - Nothing bought";
+        }
+        return name + " - " + products.stream().map(Product::getName).collect(Collectors.joining(", "));
+    }
+}
