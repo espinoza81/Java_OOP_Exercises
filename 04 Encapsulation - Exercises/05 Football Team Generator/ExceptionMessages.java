278c0a98bfc332b4fcf2daf49f82518432d5b591
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-07-01 11:50:19 +0300

    Create ExceptionMessages.java

---
+++ 04 Encapsulation - Exercises/05 Football Team Generator/ExceptionMessages.java
@@ -0,0 +1,8 @@
+package footballTeamGenerator;
+
+public class ExceptionMessages {
+    public static final String INVALID_NAME = "A name should not be empty.";
+    public static final String NO_SUCH_PLAYER = "Player %s is not in %s team.";
+    public static final String INVALID_SKILL_AMOUNT = " should be between 0 and 100.";
+    public static final String NO_SUCH_TEAM = "Team %s does not exist.";
+}
