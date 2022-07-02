e29d07a4710a5380218b84b63e5d4fca369820c2
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-07-02 14:42:50 +0300

    Create Messages.java

---
+++ 06 Inheritance - Exercises/06 Animals/Messages.java
@@ -0,0 +1,10 @@
+package animals;
+
+public class Messages {
+    public static final String INVALID_INPUT = "Invalid input!";
+    public static final String DOG_SOUND = "Woof!";
+    public static final String CAT_SOUND = "Meow meow";
+    public static final String FROG_SOUND = "Ribbit";
+    public static final String KITTEN_SOUND = "Meow";
+    public static final String TOMCAT_SOUND = "MEOW";
+}
