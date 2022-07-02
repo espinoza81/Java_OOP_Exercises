a560ca5016bae843e3eda1477bcf4b9ba2c94aaf
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-07-02 14:39:25 +0300

    Create Animal.java

---
+++ 06 Inheritance - Exercises/06 Animals/Animal.java
@@ -0,0 +1,58 @@
+package animals;
+
+public class Animal {
+
+    private String name;
+    private int age;
+    private String gender;
+
+    public Animal(String name, int age, String gender) {
+        setName(name);
+        setAge(age);
+        setGender(gender);
+    }
+
+    @Override
+    public String toString(){
+        return this.getClass().getSimpleName() + System.lineSeparator() +
+                this.name + " " + age + " " + gender + System.lineSeparator() +
+                this.produceSound();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(name.trim().isEmpty()) {
+            throw new IllegalArgumentException(Messages.INVALID_INPUT);
+        }
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if(age < 0) {
+            throw new IllegalArgumentException(Messages.INVALID_INPUT);
+        }
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        if(!("Female".equals(gender) || "Male".equals(gender))) {
+            throw new IllegalArgumentException(Messages.INVALID_INPUT);
+        }
+        this.gender = gender;
+    }
+
+    public String produceSound() {
+        return "sound";
+    }
+}
