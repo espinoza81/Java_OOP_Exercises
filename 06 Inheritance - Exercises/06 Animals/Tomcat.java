8bce944a987d2ccbe8a88d618412c3464399b3aa
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-07-04 09:35:54 +0300

    Update Tomcat.java

--- 06 Inheritance - Exercises/06 Animals/Tomcat.java
+++ 06 Inheritance - Exercises/06 Animals/Tomcat.java
@@ -1,6 +1,6 @@
 package animals;
 
-public class Tomcat extends Cat {
+public class Tomcat extends Cat implements Sound {
     private static final String GENDER = "Male";
 
     public Tomcat(String name, int age) {
