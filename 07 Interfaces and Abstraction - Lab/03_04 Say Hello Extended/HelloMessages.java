9439103b601523e435d8df411677a823cac88fa0
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-07-04 14:58:23 +0300

    Create HelloMessages.java

---
+++ 07 Interfaces and Abstraction - Lab/03_04 Say Hello Extended/HelloMessages.java
@@ -0,0 +1,7 @@
+package sayHello;
+
+public class HelloMessages {
+    public static final String BULGARIAN = "Здравей";
+    public static final String EUROPEAN = "Hello";
+    public static final String CHINESE = "Djydjybydjy";
+}
