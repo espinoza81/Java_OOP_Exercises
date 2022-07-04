a652d3ead89eb05b60cb0fb6d1605e5f4f173fb3
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-07-04 14:58:52 +0300

    Create Chinese.java

---
+++ 07 Interfaces and Abstraction - Lab/03_04 Say Hello Extended/Chinese.java
@@ -0,0 +1,13 @@
+package sayHello;
+
+public class Chinese extends BasePerson implements Person {
+
+    public Chinese(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return HelloMessages.CHINESE;
+    }
+}
