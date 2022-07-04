0d64d51838750b4b94454065f18c66b4e4d81048
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-07-04 09:35:24 +0300

    Update Dog.java

--- 06 Inheritance - Exercises/06 Animals/Dog.java
+++ 06 Inheritance - Exercises/06 Animals/Dog.java
@@ -1,6 +1,6 @@
 package animals;
 
-public class Dog extends Animal {
+public class Dog extends Animal implements Sound {
     public Dog(String name, int age, String gender) {
         super(name, age, gender);
     }
