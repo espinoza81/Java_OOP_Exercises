a3113e7f4bb01a5283e5888f8b946ac3136be915
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-07-04 13:41:03 +0300

    Create CarImpl.java

---
+++ 07 Interfaces and Abstraction - Lab/02 Car Shop Extended/CarImpl.java
@@ -0,0 +1,43 @@
+package carShop;
+
+import java.io.Serializable;
+
+public class CarImpl implements Car, Serializable{
+    private String model;
+    private String color;
+    private Integer horsePower;
+    private String countryProduced;
+
+    public CarImpl(String model, String color, Integer horsePower, String countryProduced) {
+        this.model = model;
+        this.color = color;
+        this.horsePower = horsePower;
+        this.countryProduced = countryProduced;
+    }
+
+    @Override
+    public String toString() {
+        //This is Leon produced in Spain and have 4 tires
+        return String.format("This is %s produced in %s and have %d tires%n", model, countryProduced, TIRES);
+    }
+
+    @Override
+    public String getModel() {
+        return model;
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public Integer getHorsePower() {
+        return horsePower;
+    }
+
+    @Override
+    public String countryProduced() {
+        return countryProduced;
+    }
+}
