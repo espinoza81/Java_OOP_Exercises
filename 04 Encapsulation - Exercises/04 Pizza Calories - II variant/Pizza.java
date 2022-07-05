53fdcb4577eca9d0f26e1668420d6f0e2a7c8578
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-07-05 08:37:05 +0300

    Create Pizza.java

---
+++ 04 Encapsulation - Exercises/04 Pizza Calories - II variant/Pizza.java
@@ -0,0 +1,46 @@
+package pizzaCalories;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pizza {
+    private String name;
+    private Dough dough;
+    private List<Topping> toppings;
+
+    public Pizza(String name, int numberOfToppings) {
+        setName(name);
+        setToppings(numberOfToppings);
+    }
+
+    private void setName(String name) {
+        if (name == null || name.trim().isEmpty() || name.length() > 15) {
+            throw new IllegalArgumentException(ExceptionMessages.INVALID_PIZZA_NAME);
+        }
+        this.name = name;
+    }
+
+    private void setToppings(int numberOfToppings) {
+        if (numberOfToppings < 0 || numberOfToppings > 10) {
+            throw new IllegalArgumentException(ExceptionMessages.INVALID_TOPPING_NUMBERS);
+        }
+
+        this.toppings = new ArrayList<>(numberOfToppings);
+    }
+
+    public void setDough(Dough dough) {
+        this.dough = dough;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addTopping(Topping topping) {
+        toppings.add(topping);
+    }
+
+    public double getOverallCalories() {
+        return dough.calculateCalories() + toppings.stream().mapToDouble(Topping::calculateCalories).sum();
+    }
+}
