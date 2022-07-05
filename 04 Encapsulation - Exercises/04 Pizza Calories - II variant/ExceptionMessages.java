76b08d8f1a26e87f73e2d87b3441180c7d8d7cde
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-07-05 08:36:20 +0300

    Create ExceptionMessages.java

---
+++ 04 Encapsulation - Exercises/04 Pizza Calories - II variant/ExceptionMessages.java
@@ -0,0 +1,10 @@
+package pizzaCalories;
+
+public class ExceptionMessages {
+    public static final String INVALID_PIZZA_NAME = "Pizza name should be between 1 and 15 symbols.";
+    public static final String INVALID_DOUGH_WEIGHT = "Dough weight should be in the range [1..200].";
+    public static final String INVALID_DOUGH_TYPE = "Invalid type of dough.";
+    public static final String INVALID_TOPPING_WEIGHT = "%s weight should be in the range [1..50].";
+    public static final String INVALID_TOPPING_TYPE = "Cannot place %s on top of your pizza.";
+    public static final String INVALID_TOPPING_NUMBERS = "Number of toppings should be in range [0..10].";
+}
