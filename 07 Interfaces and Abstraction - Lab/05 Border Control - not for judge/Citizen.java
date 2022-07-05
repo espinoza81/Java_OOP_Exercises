cac2ab378da5af6296522dad2bcf8b081d41671d
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-07-05 09:21:38 +0300

    Create Citizen.java

---
+++ 07 Interfaces and Abstraction - Lab/05 Border Control - not for judge/Citizen.java
@@ -0,0 +1,20 @@
+package borderControl;
+
+public class Citizen extends BasePeople {
+    private int age;
+    private String name;
+
+    public Citizen(String id, int age, String name) {
+        super(id);
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
