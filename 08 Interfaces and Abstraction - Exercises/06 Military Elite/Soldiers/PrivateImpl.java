14a9d15628feb02f5c45e1a8023e1395b53a52ea
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-07-06 15:03:15 +0300

    Create PrivateImpl.java

---
+++ 08 Interfaces and Abstraction - Exercises/06 Military Elite/Soldiers/PrivateImpl.java
@@ -0,0 +1,23 @@
+package militaryElite.Soldiers;
+
+import militaryElite.Interfaces.Private;
+
+public class PrivateImpl extends SoldierImpl implements Private {
+    private double salary;
+
+    public PrivateImpl(int id, String firstName, String lastName, double salary) {
+        super(id, firstName, lastName);
+        this.salary = salary;
+    }
+
+    @Override
+    public double getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        //"Name: {firstName} {lastName} Id: {id} Salary: {salary}"
+        return String.format("Name: %s %s Id: %d Salary: %.2f", getFirstName(), getLastName(), getId(), salary);
+    }
+}
