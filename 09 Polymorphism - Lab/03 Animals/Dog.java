63a00a14ffec3e26cc7c43a6af91eeb488646939
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-07-07 17:20:00 +0300

    Create Dog.java

---
+++ 09 Polymorphism - Lab/03 Animals/Dog.java
@@ -0,0 +1,14 @@
+package animalsPolymorh;
+
+public class Dog extends Animal{
+
+    public Dog(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    public String explainSelf() {
+        return super.explainSelf() + System.lineSeparator() +
+                "DJAAF";
+    }
+}
