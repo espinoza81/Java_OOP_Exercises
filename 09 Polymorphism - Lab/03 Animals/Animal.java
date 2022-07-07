f8c365ae70b0815e278e8706db0dca6d40ad1240
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-07-07 17:19:24 +0300

    Create Animal.java

---
+++ 09 Polymorphism - Lab/03 Animals/Animal.java
@@ -0,0 +1,23 @@
+package animalsPolymorh;
+
+public class Animal {
+    private String name;
+    private String favouriteFood;
+
+    public Animal(String name, String favouriteFood) {
+        this.name = name;
+        this.favouriteFood = favouriteFood;
+    }
+
+    public String explainSelf() {
+        return String.format("I am %s and my favourite food is %s", name, favouriteFood);
+    };
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFavouriteFood() {
+        return favouriteFood;
+    }
+}
