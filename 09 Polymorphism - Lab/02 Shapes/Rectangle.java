e4e4931f051b04f3e8f03b97d60712f57786a2c0
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-07-07 17:04:02 +0300

    Create Rectangle.java

---
+++ 09 Polymorphism - Lab/02 Shapes/Rectangle.java
@@ -0,0 +1,31 @@
+package shapes;
+
+public class Rectangle extends Shape {
+    private Double height;
+    private Double width;
+
+    public Rectangle(Double height, Double width) {
+        this.height = height;
+        this.width = width;
+        super.setArea(this.calculateArea());
+        super.setPerimeter(this.calculatePerimeter());
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * (height + width);
+    }
+
+    @Override
+    public double calculateArea() {
+        return height * width;
+    }
+
+    public final Double getHeight() {
+        return height;
+    }
+
+    public final Double getWidth() {
+        return width;
+    }
+}
