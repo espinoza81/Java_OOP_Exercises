435c7dbe3c76c582f13959bc211d8771340a880f
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-07-08 10:46:26 +0300

    Create Messages.java

---
+++ 10 Polymorphism - Exercises/01 Vehicles/Messages.java
@@ -0,0 +1,6 @@
+package vehicles;
+
+public class Messages {
+    public static final String TRAVELING = "%s travelled %s km";
+    public static final String NOT_TRAVELING = "%s needs refueling";
+}
