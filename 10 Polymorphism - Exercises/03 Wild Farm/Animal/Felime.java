e49ebae5607cdbebee013528e7d5e4972c88cbe6
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-07-10 11:32:53 +0300

    Create Felime.java

---
+++ 10 Polymorphism - Exercises/03 Wild Farm/Animal/Felime.java
@@ -0,0 +1,8 @@
+package wildFarm.Animal;
+
+public abstract class Felime extends Mammal {
+
+    public Felime(String animalName, String animalType, Double animalWeight, String livingRegion) {
+        super(animalName, animalType, animalWeight, livingRegion);
+    }
+}
