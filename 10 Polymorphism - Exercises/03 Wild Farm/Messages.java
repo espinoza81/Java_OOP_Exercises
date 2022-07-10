f3bf03cb444dd06e6b7963ba68b1d7b3c6313326
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-07-10 11:29:41 +0300

    Create Messages.java

---
+++ 10 Polymorphism - Exercises/03 Wild Farm/Messages.java
@@ -0,0 +1,9 @@
+package wildFarm;
+
+public class Messages {
+    public static final String MOUSE_SOUND = "SQUEEEAAAK!";
+    public static final String CAT_SOUND = "Meowwww";
+    public static final String TIGER_SOUND = "ROAAR!!!";
+    public static final String ZEBRA_SOUND = "Zs";
+    public static final String WRONG_FOOD = "%s are not eating that type of food!";
+}
