52192764939bcdf763c31851af0d4dc29872c698
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-07-22 06:52:06 +0300

    Add files via upload

--- 12 Reflection - Exercises/01 Harvesting Fields/Main.java
+++ 12 Reflection - Exercises/01 Harvesting Fields/Main.java
@@ -7,42 +7,53 @@ import java.util.Scanner;
 import java.util.function.Consumer;
 
 public class Main {
+
+	private final static String PRINT = "%s %s %s%n"; //{access modifier} {field type} {field name}
+	private final static String END_COMMAND = "HARVEST";
+	private final static String PRIVATE = "private";
+	private final static String PROTECTED = "protected";
+	private final static String PUBLIC = "public";
+	private final static Scanner CONSOLE = new Scanner(System.in);
+	private final static Field[] declaredFields = RichSoilLand.class.getDeclaredFields();
+
 	public static void main(String[] args) {
-		final String PRINT = "%s %s %s%n"; //{access modifier} {field type} {field name}
-		Scanner console = new Scanner(System.in);
+
 		String command;
 
-		Class reflectionClass = RichSoilLand.class;
-
-		Field[] declaredFields = reflectionClass.getDeclaredFields();
-
-		while (!"HARVEST".equals(command = console.nextLine())) {
+		while (!END_COMMAND.equals(command = CONSOLE.nextLine())) {
 
 			switch (command) {
-				case "private":
-					Arrays.stream(declaredFields).
-							filter(s -> Modifier.isPrivate(s.getModifiers())).
-							forEach(getFieldConsumer(PRINT));
+				case PRIVATE:
+					filterFieldsModifier(PRIVATE);
 					break;
-				case "protected":
-					Arrays.stream(declaredFields).
-							filter(s -> Modifier.isProtected(s.getModifiers())).
-							forEach(getFieldConsumer(PRINT));
+
+				case PROTECTED:
+					filterFieldsModifier(PROTECTED);
 					break;
-				case "public":
-					Arrays.stream(declaredFields).
-							filter(s -> Modifier.isPublic(s.getModifiers())).
-							forEach(getFieldConsumer(PRINT));
+
+				case PUBLIC:
+					filterFieldsModifier(PUBLIC);
 					break;
+
 				case "all":
 					Arrays.stream(declaredFields).
-							forEach(getFieldConsumer(PRINT));
+							forEach(getFieldConsumer());
 					break;
 			}
 		}
+
+		CONSOLE.close();
 	}
 
-	private static Consumer<Field> getFieldConsumer(String PRINT) {
+	private static void filterFieldsModifier(String aPrivate) {
+
+		Arrays.stream(declaredFields).
+				filter(s -> Modifier.toString(s.getModifiers()).equals(aPrivate)).
+				forEach(getFieldConsumer());
+	}
+
+	private static Consumer<Field> getFieldConsumer() {
+
 		return s -> System.out.printf(PRINT, Modifier.toString(s.getModifiers()), s.getType().getSimpleName(), s.getName());
 	}
 }
